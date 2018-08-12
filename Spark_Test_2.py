d3fb645e7d2b8f4dc58d6336916593f53814c3a1
mattjones <mrjones.cs@gmail.com>
2018-08-11 18:04:41 -0700

    New File

---
+++ Spark_Test_2.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+
+#Basic Setup
+conf = SparkConf().setMaster("local").setAppName("Superheros")
+sc = SparkContext(conf=conf)
+
+def countCoOccurences(line):
+    elements = line.split()
+    return (int(elements[0]), len(elements) - 1)
+
+def parseNames(line):
+    fields = line.split("\"")
+    return (int(fields[0]), fields[1].encode("utf8"))
+
+#Load data into RDD
+names = sc.textFile("/Users/mattjones/Desktop/SparkCourse/Marvel-Names.txt")
+namesRDD = names.map(parseNames)
+
+
+lines = sc.textFile("/Users/mattjones/Desktop/SparkCourse/Marvel-Graph.txt")
+pairings = lines.map(countCoOccurences)
+totalFriendsByCharacter = pairings.reduceByKey(lambda x, y: x + y)
+flipped = totalFriendsByCharacter.map(lambda x: (x[1], x[0]))
+
+mostPopular = flipped.max()
+mostPopularName = namesRDD.lookup(mostPopular[1])[0]
+print(mostPopularName)
+print(mostPopular[0])
